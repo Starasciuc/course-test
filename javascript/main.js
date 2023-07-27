5ef0b863b9b155a7652e175622337e60ac4f97d0
Starasciuc <123skaji@gmail.com>
2023-07-27 22:30:36 +0300

    this is the final code for website

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+const mobileBarsButton = document.querySelector('.mobile-header-header__wrapper i')
+const mobileNavMenuWrapper = document.querySelector('.mobile-header-nav__wrapper')
+const mobileNavMenuExitButton = document.querySelector('.mobile-header-nav__wrapper i')
+
+mobileBarsButton.addEventListener('click', function (e) {
+mobileNavMenuWrapper.style.display = 'flex'})
+
+mobileNavMenuExitButton.addEventListener('click', function (e) {
+mobileNavMenuWrapper.style.display = 'none'
+
+}
+)
